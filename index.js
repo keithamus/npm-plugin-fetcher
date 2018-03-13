4baf93d1aac5e85420a7e2e47599348e99d0f4bb
Keith Cirkel <keithamus@users.noreply.github.com>
2018-03-13 19:07:44 +0000

    complete rewrite

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 #!/usr/bin/env node
-'use strict';
-var cli = require('cli');
-var https = require('https');
-var path = require('path');
-var fs = require('fs');
+'use strict'
+var cli = require('cli')
+var https = require('https')
+var path = require('path')
+var fs = require('fs')
+var {URL} = require('url')
 
-function get(url, cb) {
-    cli.debug('Fetching url ' + url);
-    var time = Date.now();
-    https.get(url, function (res) {
-        var data = '';
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) { data += String(chunk); })
-           .on('end', function () {
-                cli.debug('Fetched: ' + url + ' (' + (Date.now() - time) + 'ms)');
-                cb(data);
-           });
-    });
+function bufferStream(stream) {
+  return new Promise((resolve, reject) => {
+    const chunks = []
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.on('end', () => resolve(Buffer.concat(chunks)))
+    stream.on('error', reject)
+  })
+}
+
+function getJSON(url) {
+  return new Promise((resolve) => https.get(url, resolve))
+    .then(bufferStream)
+    .then(JSON.parse)
+}
+
+async function getAllPages(urlBase) {
+  const url = new URL(urlBase)
+  url.searchParams.set('size', 1)
+  url.searchParams.set('from', 0)
+  cli.debug(`Getting first page: ${url}`)
+  const firstPage = await getJSON(url)
+  const pageCount = Math.ceil(firstPage.total / 20)
+  const rest = await Promise.all(
+    new Array(pageCount).fill(url).map((urlBase, i) => {
+      const url = new URL(urlBase)
+      url.searchParams.set('size', 20)
+      url.searchParams.set('from', i * 20 + 1)
+      cli.debug(`Getting page ${i}: ${url}`)
+      return getJSON(url)
+    })
+  )
+  return rest.reduce((total, page) => total.concat(page.objects), firstPage.objects)
 }
 
 cli.enable('version', 'autocomplete', 'help', 'status', 'glob')
@@ -41,52 +62,41 @@ cli.enable('version', 'autocomplete', 'help', 'status', 'glob')
             'string',
             './'
         ]
-    });
+    })
 
 
 if (!cli.args.length) {
-    cli.debug('No keyword given, showing help');
-    cli.getUsage(1);
+    cli.debug('No keyword given, showing help')
+    cli.getUsage(1)
 }
-var keyword = cli.args[0];
-cli.debug('Finding packages with keyword ' + keyword);
-var url = [
-    cli.options.registry,
-    '/-/_view/byKeyword?startkey=%5B%22',
-    keyword,
-    '%22%5D&endkey=%5B%22',
-    keyword,
-    '%22,%7B%7D%5D&group_level=2'
-].join('');
+const keyword = cli.args[0]
+cli.debug('Finding packages with keyword ' + keyword)
 
-var blacklist = [];
+let blacklist = []
 if (cli.options.blacklist) {
-    blacklist = require(path.resolve(process.cwd(), cli.options.blacklist));
+    blacklist = require(path.resolve(process.cwd(), cli.options.blacklist))
 }
 
-get(url, function (plugins) {
-    plugins = JSON.parse(plugins).rows.map(function (row) {
-        return row.key[row.key.length - 1];
-    });
-    cli.debug('Found the following plugins: \n\t' + plugins.join('\n\t'));
-    plugins = plugins.filter(function (item) {
-        if (blacklist.indexOf(item) === -1) {
-            return true;
-        } else {
-            cli.debug('Plugin: "' + item + '" has been blacklisted');
-            return false;
-        }
-    });
-    cli.info('Fetching data on these plugins: \n\t' + plugins.join('\n\t'));
-    plugins.forEach(function (name) {
-        get(cli.options.registry + '/' + name, function (packagejson) {
-            var file = path.resolve(process.cwd(), cli.options.out, name + '.json');
-            cli.debug('Writing file ' + file);
-            var time = Date.now();
-            fs.writeFile(file, packagejson, function (err) {
-                if (err) { throw err; }
-                cli.info('Written: ' + file + ' (' + (Date.now() - time) + 'ms)');
-            });
-        });
-    });
-});
+const url = `${cli.options.registry}/-/v1/search?text=keywords%3A${keyword}`
+getAllPages(url).then(async plugins => {
+  plugins = plugins.map(plugin => plugin.package.name)
+  cli.debug('Found the following plugins: \n\t' + plugins.join('\n\t'))
+  plugins = plugins.filter(name => {
+    const isBlacklisted = blacklist.includes(name)
+    if (isBlacklisted) cli.debug('Plugin: "' + name + '" has been blacklisted')
+    return !isBlacklisted
+  })
+  cli.info('Fetching data on these plugins: \n\t' + plugins.join('\n\t'))
+  return Promise.all(plugins.map(async name => {
+    const res = await new Promise(resolve => https.get(`${cli.options.registry}/${name.replace(/\//g, '%2F')}`, resolve))
+    const file = path.resolve(process.cwd(), cli.options.out, `${name.replace(/[^a-z]/g, '_')}.json`)
+    const fileWrite = fs.createWriteStream(file)
+    cli.debug('Writing file ' + file)
+    res.pipe(fileWrite)
+    await new Promise((resolve, reject) => fileWrite.on('end', resolve).on('error', reject))
+    cli.info('Written: ' + file + ' (' + (Date.now() - time) + 'ms)')
+  }))
+}).catch(e => {
+  console.error(e.stack || e)
+  process.exit(1)
+})
